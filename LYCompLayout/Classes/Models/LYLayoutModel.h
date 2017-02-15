c28d45e60520bec77fd4dd701b37523553939cfe
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-02-15 16:41:08 +0800

    Add : layout model base class

---
+++ LYCompLayout/Classes/Models/LYLayoutModel.h
@@ -0,0 +1,13 @@
+//
+//  LYLayoutModel.h
+//  LYCOMPLAYOUT
+//
+//  CREATED BY BLODELY ON 2017-02-15.
+//  COPYRIGHT (C) 2017 BLODELY. ALL RIGHTS RESERVED.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYLayoutModel : NSObject
+
+@end
---
+++ LYCompLayout/Classes/Models/LYLayoutModel.m
@@ -0,0 +1,13 @@
+//
+//  LYLayoutModel.m
+//  LYCOMPLAYOUT
+//
+//  CREATED BY BLODELY ON 2017-02-15.
+//  COPYRIGHT (C) 2017 BLODELY. ALL RIGHTS RESERVED.
+//
+
+#import "LYLayoutModel.h"
+
+@implementation LYLayoutModel
+
+@end
