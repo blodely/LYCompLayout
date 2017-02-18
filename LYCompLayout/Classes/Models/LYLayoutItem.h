1862abd4de87188909f120fb40a7ce87c90ab2c0
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-02-18 14:08:55 +0800

    Modify : design layout item properties

--- LYCompLayout/Classes/Models/LYLayoutItem.h
+++ LYCompLayout/Classes/Models/LYLayoutItem.h
@@ -10,4 +10,45 @@
 
 @interface LYLayoutItem : LYLayoutModel
 
+/**
+ Priority in horizontal
+ */
+@property (nonatomic, assign) NSUInteger priorityHorizontal;
+
+/**
+ Priority in vertical
+ */
+@property (nonatomic, assign) NSUInteger priorityVertical;
+
+/**
+ Units in horizontal
+ */
+@property (nonatomic, assign) NSInteger unitHorizontal;
+
+/**
+ Units in vertical
+ */
+@property (nonatomic, assign) NSInteger unitVertical;
+
+/**
+ Item's width
+ 
+ for now, width is read only
+ */
+@property (nonatomic, assign, readonly) CGFloat width;
+
+/**
+ Item's height
+ 
+ for now, height is read only
+ */
+@property (nonatomic, assign, readonly) CGFloat height;
+
+/**
+ Convert self to NSDictionary data for plist persistence
+
+ @return dictionary value
+ */
+- (NSDictionary *)toDictionary;
+
 @end
